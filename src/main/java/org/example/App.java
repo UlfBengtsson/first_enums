60945cdbd7a66f06ebce9cb89fd2576e29a29497
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-07-13 11:14:12 +0200

    more ex

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,36 +2,65 @@ package org.example;
 
 import org.example.Temp;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput = scanner
+                .nextLine()
+                .trim()
+                .toLowerCase()
+                .replace(" ", "");
+
+
+        Temp show = null;
+
+        switch (userInput) {
+            case "outsidescope":
+                show = Temp.OutsideScope;
+                break;
+            case "steam":
+                show = Temp.Steam;
+                break;
+            case "fluid":
+                show = Temp.Fluid;
+                break;
+            case "frozen":
+                show = Temp.Frozen;
+                break;
+            default:
+                System.out.println("not correct input, defaulting to OutsideScope");
+                show=Temp.OutsideScope;
+                break;
+        }
+        System.out.println(show);
+    }
+
+    static void waterBoilerEx() {
         WaterBoiler waterBoiler = new WaterBoiler(155.5);
 
         System.out.println(waterBoiler.getStateOfWater());
+    }
 
-        Object steamingHot = new WaterBoiler(999);
+    static void riskOfNotCheckingBeforeCasting() {
+        //bad way that can crash our code
+        Object obj = new Object();
+        System.out.println(((WaterBoiler)obj).getStateOfWater());//Exception in thread "main" java.lang.ClassCastException: java.lang.Object cannot be cast to org.example.WaterBoiler
+    }
+
+    static void checkingBeforeCast() {
+
+        Object steamingHot =new WaterBoiler(999);//new Object();
 
         if (steamingHot instanceof WaterBoiler) {
-            System.out.println( ((WaterBoiler) steamingHot).getStateOfWater() );
+            System.out.println("State of the water inside the boiler: " + ((WaterBoiler) steamingHot).getStateOfWater() );
         }
-
-        Temp show = Temp.Frozen;
-
-        switch (show) {
-            case OutsideScope:
-
-                break;
-            case Steam:
-
-                break;
-            case Fluid:
-
-                break;
-            case Frozen:
-
-                break;
-
+        else {
+            System.out.println("Was not of the WaterBoiler family");
         }
     }
 }
